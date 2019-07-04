eadd69c903f477b75938223308451d7032882555
abcwjf123456 <2368974010@qq.com>
2019-07-04 09:53:51 +0800

    更新

---
+++ python/The-first-chapter/variable.py
@@ -0,0 +1,20 @@
+# print("Hello python world")
+
+
+# message="Hello python world"
+# print(message)
+# message="Hello python crash course world"
+# print(message)
+# python只记录变量最新值，可随时在程序中改变变量值
+# 变量名抒写规范尽量使用小写
+
+
+# message="Hello python world"
+# print(mesage)
+# 常见错误（名称错误）通常有两种情况：1使用变量名之前忘记给它赋值 2输入变量名时拼写错误
+# 解释器会提供Traceback记录
+# Traceback (most recent call last):
+#   File "E:/4000hours/python/The-first-chapter/variable.py", line 13, in <module>
+#     print(mesage)
+# NameError: name 'mesage' is not defined
+
